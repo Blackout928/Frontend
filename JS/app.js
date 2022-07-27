83b72068f622f04a3ba1e86b694305b5a45f24d6
Will <will.roche008@gmail.com>
2022-07-27 21:21:50 +1000

    added booleans, integers and index experiment

--- JS/app.js
+++ JS/app.js
@@ -1,2 +1,36 @@
-console.log('fortnite battle pass')
-document.write('nigeria')
+
+
+let phrase = "exynosisbad";
+
+let phraseLength = phrase.length
+
+if (phraseLength > 10) {
+    console.log('your phrase is approved!');
+}
+
+else{
+    console.log('Your string isnt long enough')
+}
+// experiment with index
+console.log('Hello World!'[10])
+
+let name = "Doofenshmirtz";
+console.log(name[name.length -1])
+
+//integers
+
+let num = 3;
+console.log(typeof num)
+
+console.log(num + 3);
+
+// booleans 
+/* note to self Null means the variable is intentionally 0 whilst 
+undefined means a value was never set in the first place*/ 
+console.log(5 > 10)
+console.log(null >= 0)
+
+// Variables
+/*note to self: make your variable names descriptive and 
+not bland. instead of "camels" do "numberOfCamels" or instead of name try myName*/
+let myName = "Will"
