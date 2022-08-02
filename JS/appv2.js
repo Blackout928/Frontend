530a9da5fb3d2b10da1c73b09d8391ab9323dac1
Will <will.roche008@gmail.com>
2022-08-02 19:46:09 +1000

    added return keyword, started functions and finished loops

--- JS/appv2.js
+++ JS/appv2.js
@@ -44,7 +44,7 @@
 
 //solution
 
-for (let i = 1; i <= 20; i++) {
+for (let i = 1; i <= 50; i++) {
     if (i % 5 === 0 && i % 3 === 0) {
         console.log(`${i} --> Frontend Simplified`)
     }
@@ -55,7 +55,67 @@ for (let i = 1; i <= 20; i++) {
     else if (i % 5 === 0) {
         console.log(`${i} --> Simplified`)
     }
+    else if (i % 8 === 0) {
+        console.log(`${i} --> fortnite battle pas`)
+    }
     else {
-        console.log(i)
+        console.log(i) 
     }
 }
+
+//fard thingy 
+//print out every character in a string
+
+let phrase = "Frontend Simplified"
+
+for (let i = 0; i <= phrase.length; i++) {
+    console.log(phrase[i]);
+}
+
+//functions 
+//this is actually fun
+
+//DRY - Don't repeat yourself
+
+console.log("welcome to amogus, David")
+console.log("welcome to amogus, Xav")
+console.log("welcome to amogus, Alah Akbah")
+
+// no, very bad
+
+
+//function definiton
+function welcomePerson(name) {
+    console.log(`Welcome to Amogus, ${name}`)
+}
+// call the function
+welcomePerson("jahamae")
+welcomePerson("fortiniiitie")
+welcomePerson("fortiniiitie pubagee")
+
+
+//here is used more variables
+
+function userid(firstName, lastName, job, numberid) {
+    console.log(`Hello, ${firstName} ${lastName}. You are ${job}. Your ID is ${numberid}`)
+}
+// separate strings with comma
+userid("Olivia", "Pierce", "The Regional Manager Of the UAC", "39436X")
+
+//return keyword
+
+function fn() {
+    return '5'
+    console.log('MA BALLS')
+    
+}
+
+console.log(fn())
+
+//return make the function equal to the return value
+
+function sumOfTwoNumbers(num1, num2) {
+    return num1 + num2
+}
+
+console.log(sumOfTwoNumbers(33333, 34325))
